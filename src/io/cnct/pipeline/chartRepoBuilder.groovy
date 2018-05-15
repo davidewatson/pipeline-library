1a331653178a83fd1837b8c276472c4db764ba5c
David E Watson <davidewatson@gmail.com>
2018-05-15 14:10:31 -0700

    Delete test pods upon completion.

--- src/io/cnct/pipeline/chartRepoBuilder.groovy
+++ src/io/cnct/pipeline/chartRepoBuilder.groovy
@@ -994,7 +994,7 @@ def helmTestHandler(scmVars) {
       for (chart in pipeline.deployments) {
         if (chart.chart) {
           def commandString = """
-          helm test --tiller-namespace ${pipeline.helm.namespace} --timeout ${chart.timeout} ${chart.release}-${kubeName(env.JOB_NAME)} --debug
+          helm test --tiller-namespace ${pipeline.helm.namespace} --timeout ${chart.timeout} ${chart.release}-${kubeName(env.JOB_NAME)} --debug --cleanup
           """ 
 
           retry(chart.retries) {
